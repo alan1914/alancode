f952889cfcffaea05a4290a2f0f3f343b20d90bd
stone <db0n15@163.com>
2018-12-29 16:05:50 +0800

    <feat>(<$leetcode>):<二叉树的最小深度 求逆波兰表示法>

---
+++ src/main/java/com/alan/leetcode/EvaluateReversePolishNotation.java
@@ -0,0 +1,59 @@
+package com.alan.leetcode;
+
+import java.util.Stack;
+
+/**
+ * @author stone
+ * @des 求逆波兰表示法
+ * @date 2018/12/29/029 15:23
+ **/
+public class EvaluateReversePolishNotation {
+
+    public static int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<Integer>();
+        for (String str : tokens) {
+            if ("-".equals(str) || "+".equals(str) || "*".equals(str) || "/".equals(str)) {
+                int b = stack.pop();
+                int a = stack.pop();
+
+                stack.push(calculate(str, a, b));
+            } else {
+                stack.push(Integer.valueOf(str));
+            }
+        }
+        return stack.elementAt(0);
+    }
+
+    public static int calculate(String opr, int a, int b) {
+        int result = 0;
+        switch (opr) {
+            case "-":
+                result = a - b;
+                break;
+            case "+":
+                result = a + b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                if (b == 0) {
+                    throw new RuntimeException();
+                }
+                result = a / b;
+                break;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] tokens = {"2", "1", "+", "3", "*"};
+        int result = evalRPN(tokens);
+
+        System.out.println(result);
+
+
+    }
+
+
+}
---
+++ src/main/java/com/alan/leetcode/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,46 @@
+package com.alan.leetcode;
+
+/**
+ * @author stone
+ * @des 二叉树的最小深度
+ * @date 2018/12/29/029 14:52
+ **/
+public class MinimumDepthOfBinaryTree {
+
+    /**
+     * Definition for binary tree
+     */
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * 获取最小深度
+     *
+     * @param root
+     * @return
+     */
+    public int run(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.right == null && root.left == null) {
+            return 1;
+        }
+        if (root.right == null) {
+            return run(root.left) + 1;
+        }
+        if (root.left == null) {
+            return run(root.right) + 1;
+        }
+        return Math.min(run(root.left), run(root.right)) + 1;
+    }
+
+
+}
