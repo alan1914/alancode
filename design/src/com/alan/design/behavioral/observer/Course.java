dbdec74b79a7d0847b13b7e384ee2d6046181f04
db0n15 <db0n15@163.com>
2019-04-01 18:48:30 +0800

    <feat>(<$desing>):<观察者模式>

---
+++ design/src/com/alan/design/behavioral/observer/Course.java
@@ -0,0 +1,23 @@
+package com.alan.design.behavioral.observer;
+
+import java.util.Observable;
+
+public class Course extends Observable {
+
+    private String courseName;
+
+    public Course(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void productQuestion(Course course, Question question) {
+        System.out.println(question.getUserName() + "在" + course.courseName + "提交了一个问题");
+        setChanged();
+        notifyObservers(question);
+    }
+
+}
