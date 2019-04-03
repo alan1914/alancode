5f1249d2a2337aa5c671b6bdfca144ed9dfcf65c
db0n15 <db0n15@163.com>
2019-04-03 12:25:32 +0800

    <feat>(<$design>):<备忘录模式>

---
+++ design/src/com/alan/design/behavioral/memento/ArticleMementoManager.java
@@ -0,0 +1,18 @@
+package com.alan.design.behavioral.memento;
+
+import java.util.Stack;
+
+public class ArticleMementoManager {
+
+    private final Stack<ArticleMemento> ARTICLE_MEMENTO_STACK = new Stack<>();
+
+    public ArticleMemento getMemento() {
+        ArticleMemento articleMemento = ARTICLE_MEMENTO_STACK.pop();
+        return articleMemento;
+    }
+
+    public void addMemento(ArticleMemento artivleMemeto) {
+        ARTICLE_MEMENTO_STACK.push(artivleMemeto);
+    }
+
+}
